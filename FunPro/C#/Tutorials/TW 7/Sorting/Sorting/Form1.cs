7a7a88074aac93fabbfa9bc89710e0d0a320ce6c
Vasiliy Kuznetsov <vasiliy.kuznetsov@aurea.com>
2018-10-13 21:01:46 +0500

    TW 7

---
+++ FunPro/C#/Tutorials/TW 7/Sorting/Sorting/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Sorting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void ShowMessage(string text)
+        {
+            MessageBox.Show(text, "Sorting");
+        }
+
+        private bool IsNumeric(string text)
+        {
+            return double.TryParse(text, out _);
+        }
+    }
+}
