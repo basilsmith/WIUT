38ec8c2261ba6068ba39aaafd35fa8ba05effc0b
Vasiliy Kuznetsov <30899407+vkuznetsov-aurea@users.noreply.github.com>
2020-02-01 14:34:40 +0500

    TW 16

---
+++ FunPro/C#/Tutorials/TW 16/WIUT/WIUT.DAL/Course.cs
@@ -0,0 +1,19 @@
+﻿namespace WIUT.DAL
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Course()
+        {
+
+        }
+
+        public Course(string name)
+        {
+            Name = name; 
+        }
+    }
+}
