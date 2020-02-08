2040f29504949c3a5769d8f2f90693f1a9d51026
Vasiliy Kuznetsov <30899407+vkuznetsov-aurea@users.noreply.github.com>
2020-02-08 22:13:28 +0500

    TW 17

---
+++ FunPro/C#/Tutorials/TW 17/WIUT/WIUT/ParentForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WIUT
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new AboutForm();
+            form.ShowDialog();
+        }
+    }
+}
